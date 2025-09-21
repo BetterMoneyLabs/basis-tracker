8b37bb3262b050b3c84aeda3e6caabee04b927db
kushti <kushtech@yahoo.com>
2025-09-21 20:29:44 +0300

    fixing test_create_note.sh

--- test_create_note.sh
+++ test_create_note.sh
@@ -44,17 +44,15 @@ cat /tmp/test_note.json
 echo ""
 echo ""
 
-echo "Response: (curl command would be used here)"
-echo "To test manually, run:"
-echo "curl -X POST http://localhost:3000/notes \\"
-echo "  -H \"Content-Type: application/json\" \\"
-echo "  -d '$(cat /tmp/test_note.json)'"
+echo "Response:"
+curl -X POST http://localhost:3000/notes \
+  -H "Content-Type: application/json" \
+  -d "$(cat /tmp/test_note.json)"
 echo ""
 echo ""
 
 echo "Testing get notes by issuer endpoint:"
-echo "To test manually, run:"
-echo "curl http://localhost:3000/notes/issuer/010101010101010101010101010101010101010101010101010101010101010101"
+curl http://localhost:3000/notes/issuer/010101010101010101010101010101010101010101010101010101010101010101
 
 echo ""
 echo ""
